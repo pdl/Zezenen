9692ceac7c5a368d4cd0605cece1c214ef26ab23
pdl <perrettdl@googlemail.com>
2012-01-11 22:00:21 +0000

    Copied working RecDescent parser from draft version.

--- lib/Parse/Zezenen/RecDescent.pm
+++ lib/Parse/Zezenen/RecDescent.pm
@@ -3,6 +3,12 @@ package Parse::Zezenen::RecDescent;
 use 5.006;
 use strict;
 use warnings;
+use Regexp::Common;
+use Parse::RecDescent;
+$Parse::RecDescent::skip = '';
+$::RD_HINT = 1;
+$::RD_WARN = 1;
+
 
 =head1 NAME
 
@@ -13,41 +19,159 @@ Parse::Zezenen::RecDescent - The great new Parse::Zezenen::RecDescent!
 Version 0.01
 
 =cut
-
 our $VERSION = '0.01';
+our $sGrammarItems = [
+	#	qname: /[A-Za-z][\\w\\-]*/
 
+q`
+	qname: /([A-Za-z][\\w\\-]*:)?[A-Za-z][\\w\\-]*/
+`,
+q`
+	horizontal_space: /[\\t\\x20]/ 
+	line_plus_any_indent: /(?:\\r\\n|\\n|\\r)[\\t\\x20]*/
+	non_space_text: /[^{}!\\s\\t\\r\\n]+/
+	any_space: /[\\r\\t\\n\\x20]/
+`,
+q`
+	textcurlies: (/(?<!\\{)\\{+/ | /(?<!\\})\\}+/)
+	{
+		$arg{curlies} = 0 unless defined $arg{curlies};
+		$return = $item[-1];
+		if (length ($return) >= $arg{curlies})
+		{
+			$return = undef;
+		}
+	}
+`,
+q`
+	node: (block[%arg] | textcurlies[%arg] | non_space_text[%arg] | line_plus_any_indent[%arg] | horizontal_space[%arg])
+`,
+q`
+	blockcontent: /(?<!\\{)\\{+(?!\\{)/ node[%arg, 'curlies'=>length($item[1])](s?) /(?<!\\})\\}+/ 
+	{
+		$return = $item[2];
+		if (defined $arg{curlies})
+		{
+			if (length ($item[1]) ne length ($item[-1]) or length ($item[1]) < $arg{curlies})
+			{
+				$return = undef;
+			}
+		}
+	}
+`,
+q`
+	block: selector any_space(s?) blockcontent[%arg]
+	{
+		my $content = $item{blockcontent}; # map {@{$_} if ref $_ eq ref []}
+		$return = {%{$item{selector}}, '~'=>$content};
+	}	
 
-=head1 SYNOPSIS
+`,
+q`
+	selector_class: '.' qname {$return={class=>$item{qname},};}
+	positive_integer: /[0-9]*[1-9][0-9]*/
+	selector_iterator: '*' positive_integer {$return={'#iterate'=>$item{positive_integer},};}
+	selector_id: '#' qname {$return={id=>$item{qname},};}
+	single_quoted_string: /`.$RE{delimited}{-delim=>"'"}.q`/ {$return = substr($item[1],1,length ($item[1])-2);}
+	double_quoted_string: /`.$RE{delimited}{-delim=>'"'}.q`/ {$return = substr($item[1],1,length ($item[1])-2);}
+	selector_attr_value: (single_quoted_string | double_quoted_string | /\w+/)
+	selector_attr_specified: '[' qname '=' selector_attr_value ']' {$return={$item{qname}=>$item{selector_attr_value}};}
+	selector_attr_unspecified: '[' qname ']' {$return={$item{qname}=>''};}
+	selector_attr: (selector_attr_unspecified | selector_attr_specified)
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Parse::Zezenen::RecDescent;
-
-    my $foo = Parse::Zezenen::RecDescent->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+	selector_multi: ( selector_attr | selector_class )
+	directive_marker: '!'
+	element_or_directive_name: directive_marker(?) qname 
+	{
+		$return = {};
+		$return->{'#directive'} = 1 if defined ($item{directive_marker});
+		$return->{'#name'}=$item{qname};
+		
+	}
+	selector: element_or_directive_name(1) selector_multi(s?) selector_id(?) selector_multi(s?) selector_iterator(?)
+	{
+		my $var = {};
+		shift @item;
+		foreach my $item (map {@{$_}} @item)
+		{
+			
+			foreach my $key (keys %{$item}) 
+			{
+				if ($key eq 'class' and defined $var->{class} and $var->{class} ne '')
+				{
+					$var->{class}.=' '.$item->{$key}
+				}
+				else
+				{
+					$var->{$key} = $item->{$key};
+				}
+			}
+		}
+		if (defined $var->{class} and $var->{class} =~ /\s/)
+		{
+			$var->{class} = join (' ', (sort {lc $a cmp lc $b} split (/\s/, $var->{class})));
+		}
+		$return=$var;
+	}
+`
+];
+sub new
+{
+	my $class = shift;
+	my $sGrammar = &_make_grammar;
+	my $self = 
+	{
+		'parser' => Parse::RecDescent->new($sGrammar)
+	};
+	bless $self, $class;
+}
+sub _flatten
+{
+	my $aoa = shift;
+	my $flat = [];
+	foreach (@{$aoa})
+	{
+		if (ref $_ eq ref [])
+		{
+			push @$flat, @{$_}; 
+		}
+		else
+		{
+			push @$flat, $_; 
+		}
+	}
+	return $flat;
+}
+sub _stringify_arrayref
+{
+	my $array = _flatten shift;
+	my $s = '';
+	foreach (@$array)
+	{
+		$s .= $_ if ref ($_) eq ref '';
+		$s .= ${$_} if ref ($_) eq ref \'';
+	}
+	return $s;
+}
+sub _make_grammar
+{
+	return 	_stringify_arrayref ($sGrammarItems);
+}
+sub parse
+{
+	my ( $self , $sType, $sToParse, $args) = @_;
+	$args = {} unless defined $args;
+	if (defined $sType)
+	{
+		return $self->{'parser'}->$sType($sToParse, %$args);
+	}
+	return undef;
 }
 
-=head2 function2
 
-=cut
 
-sub function2 {
-}
+
+1; # End of Parse::Zezenen::RecDescent
 
 =head1 AUTHOR
 
@@ -108,4 +232,3 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1; # End of Parse::Zezenen::RecDescent
